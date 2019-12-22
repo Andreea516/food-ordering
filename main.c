10d6913a1caf4c98f9c1b9b246c046901c6b24f5
Andreea Ilie <Andreea.Ilie@student.utcluj.ro>
2019-12-22 23:13:16 +0200

    Refactor 2

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include "customer.h"
 #include "order.h"
@@ -18,15 +17,6 @@
 
 int main() {
 
-    CUSTOMER c = createCustomer();
-
-    int cutleryChoice=0, foodChoice, typeChoice, drinkChoice, state=0, orderConfirmed=0;
-    char addInfo[MAX_ADD_INFO];
-
-    int noOfFoods,noOfDrinks; char s[MAX_LINE];
-
-    FOOD *food;
-
     FILE *data;
     data = fopen(FILE_PATH, "r");
     if(data==NULL)
@@ -35,56 +25,18 @@ int main() {
         data=stdin;
     }
 
-    //read food
-    /*
-    fgets(s,MAX_LINE, data);
-    sscanf(s, "%d", &noOfFoodTypes);
-    foodTypes = (char**)malloc(noOfFoodTypes * sizeof(char*));
-    specificFoodTypes = (char***)malloc(noOfFoodTypes * sizeof(char**));
-    specificFoodPrices = (double**)malloc(noOfFoodTypes * sizeof(double*));
-    noOfSpecificTypes = (int*)malloc(noOfFoodTypes * sizeof(int));
-    for(int i=0; i<=noOfFoodTypes-1; i++) {
-        foodTypes[i] = (char *) malloc(MAX_FOOD_NAME * sizeof(char));
-        fgets(s,MAX_LINE, data);
-        p = strtok(s, " ");
-        strcpy(foodTypes[i], p);
-        p = strtok(NULL, ":");
-        sscanf(p,"%d",&noOfSpecificTypes[i]);
-        specificFoodTypes[i] = (char **) malloc(noOfSpecificTypes[i] * sizeof(char *));
-        specificFoodPrices[i] = (double *) malloc(noOfSpecificTypes[i] * sizeof(double));
-        for( int j=0; j<noOfSpecificTypes[i]; j++)
-        {
-            p = strtok(NULL, "-");
-            specificFoodTypes[i][j] = (char *) malloc(MAX_FOOD_NAME * sizeof(char));
-            strcpy(specificFoodTypes[i][j], p+2);
-            specificFoodTypes[i][j][strlen(p) - 1] = '\0';
-            p = strtok(NULL, ")");
-            sscanf(p, "%lf", &specificFoodPrices[i][j]);
-        }
-    }
-*/
-    readFood(data, &noOfFoods,food);
-    //read drink
-    /*
-    fgets(s,MAX_LINE, data);
-    sscanf(s, "%d", &noOfDrinks);
-    drinks = (char**)malloc(noOfDrinks * sizeof(char*));
-    drinkPrices = (double*)malloc(noOfDrinks * sizeof(double));
-    fgets(s,MAX_LINE, data);
-    strrev(s);
-    p = strtok(s, "-");
-    for (int i = noOfDrinks - 1; i >= 0; i--)
-    {
-        if (i != noOfDrinks - 1)
-            p = strtok(NULL, "-");
-        sscanf(strrev(p), "%lf", &drinkPrices[i]);
-        drinks[i] = (char *) malloc(MAX_DRINK_NAME * sizeof(char));
-        p = strtok(NULL, "(");
-        strcpy(drinks[i], p);
-        strrev(drinks[i]);
-    }
-    */
-    DRINK *drink = (DRINK *)malloc(noOfDrinks * sizeof(DRINK));
+    CUSTOMER c = createCustomer();
+
+    int cutleryChoice=0, foodChoice, typeChoice, drinkChoice, state=0, orderConfirmed=0;
+    char addInfo[MAX_ADD_INFO];
+
+    int noOfFoods,noOfDrinks;
+
+    FOOD *food;
+    readFoodAndTypes(data, &noOfFoods,&food);
+
+    DRINK *drink;
+    readDrink(data, &noOfDrinks, &drink);
 
     enum State {
         SIGN_IN_UP, CHOOSE_FOOD, CHOOSE_TYPE, CHOOSE_DRINK, CHOOSE_CUTLERY, ADD_INFO
@@ -126,7 +78,8 @@ int main() {
                 confirm_order(&orderConfirmed, c, &state);
                 break;
             }}}
-
+    freeFood(food,noOfFoods);
+    freeDrinks(drink, noOfDrinks);
     fclose(data);
     return 0;
 }
